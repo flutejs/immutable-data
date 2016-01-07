30c32c7aaa9269ce9f0f31aa7344aef23a8d008e
slientcloud <rjmuqiang@gmail.com>
2016-01-07 17:47:09 +0800

    add tests

--- src/__tests__/object-test.js
+++ src/__tests__/object-test.js
@@ -2,8 +2,32 @@ var set = require('../index');
 
 describe('object', () => {
 
+  it('should throw error if data is not an object', () => {
+    expect(() => set('a', {})).to.throw('data should be Object or Array');
+  });
+
+  it('should return data if obj not set', () => {
+    var obj1 = set({
+      "a": 1
+    });
+    expect(obj1).to.eql({'a': 1});
+
+    var obj2 = set(['a']);
+    expect(obj2).to.eql(['a']);
+  });
+
+  it('should return data if obj keys === 0', () => {
+    var obj1 = set({
+      "a": 1
+    }, {});
+    expect(obj1).to.eql({'a': 1});
+
+    var obj2 = set(['a'], []);
+    expect(obj2).to.eql(['a']);
+  });
+
   it('object property', () => {
-    
+
     var obj1 = {
       a:{
         b:[{},{}]
@@ -22,23 +46,23 @@ describe('object', () => {
 
     expect(obj2.a.b[1]).to.equal(obj1.a.b[1]);
     expect(obj2.a.b.c).to.equal(obj1.a.b.c);
-    expect(obj2.a.b.d).to.equal('d');
-    
+    expect(obj2.d).to.equal('d');
+
   });
 
   it('array property', () => {
-    
-    var array1 = [{}]
+
+    var array1 = [{}];
 
     var obj = {};
-    var array2 = set(obj1, {
+    var array2 = set(array1, {
       "1":obj,
     });
 
     expect(array2).to.not.equal(array1);
     expect(array2[0]).to.equal(array1[0]);
     expect(array2[1]).to.equal(obj);
-    
+
   });
 
 
